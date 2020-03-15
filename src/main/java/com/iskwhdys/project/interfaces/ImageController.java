efbc0f350b0a535cf9e3e14f85cafdb6959d2809
iskwhdys <iskwhdys@gmail.com>
2020-03-15 11:20:46 +0900

    SSL有効化

--- src/main/java/com/iskwhdys/project/interfaces/ImageController.java
+++ src/main/java/com/iskwhdys/project/interfaces/ImageController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.iskwhdys.project.application.BroadcasterImageService;
 import com.iskwhdys.project.application.ChannelImageService;
 import com.iskwhdys.project.application.VideoThumbnailService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/image")
 public class ImageController {
